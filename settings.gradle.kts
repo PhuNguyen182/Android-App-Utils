67c9827b7f5ef40ff69384e3e7f809410198fae4
PhuNguyen182 <nguyenhuuphu1882.a@gmail.com>
2025-07-20 15:35:57 +0700

    Update gradle.kts files and libs.version

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,13 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        google()               // For Android and Google plugins
+        gradlePluginPortal()   // For toolchain and Kotlin plugins
+        mavenCentral()
     }
+}
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
